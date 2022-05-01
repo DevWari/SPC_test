dcae0de745bc1fc16646a00278868310717b1d07
MichaelWang <lyvanna1987@outlook.com>
2022-05-02 01:01:55 +0800

    update validation

--- app/controllers/video.controller.js
+++ app/controllers/video.controller.js
@@ -5,6 +5,14 @@ const MetaData = db.metadata;
 
 exports.getTotalVideoSize = async (req, res) => {    
   const name = req.query.name
+  // Validate request
+  if (!name) {
+    res.status(400).send({
+      status: false,
+      message: "user name can not be empty!"
+    });
+    return;
+  }
   try {
     const data = await User.findOne({ where: { name: name} })
     if (data) {
@@ -33,6 +41,15 @@ exports.getTotalVideoSize = async (req, res) => {
 
 exports.getMetaData = async(req, res) => {  
   const videoId = req.query.video_id   
+
+  // Validate request
+  if (!videoId) {
+    res.status(400).send({
+      status: false,
+      message: "video id can not be empty!"
+    });
+    return;
+  }
   try {    
     const data = await Video.findOne({ where: { video_id: videoId}, include: ['user', 'metadata']})      
     if (data) {     
@@ -63,6 +80,15 @@ exports.updateMetaData = async(req, res) => {
   const videoId = req.query.video_id  
   const videoSize = req.query.video_size  
   const viewerCount = req.query.viewer_count   
+
+  // Validate request
+  if (!videoId || !videoSize || !viewerCount) {
+    res.status(400).send({
+      status: false,
+      message: "video_id, video_size and viewer_count can not be empty!"
+    });
+    return;
+  }
   
   try {    
     const videoData = await Video.findOne({ where: { video_id: videoId}, include: ['user', 'metadata']})
