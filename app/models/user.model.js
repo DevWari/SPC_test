38612f3b5e92ceb3f82fc360d7e800cbc92a0a2c
MichaelWang <lyvanna1987@outlook.com>
2022-05-02 01:23:23 +0800

    update ReadMe

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -7,6 +7,18 @@ module.exports = (sequelize, Sequelize) => {
         type: Sequelize.INTEGER
       }
     });
+
+    User.sync().then(() => {
+      User.create({
+        name: 'Michael',
+        total_size: 40
+      });
+      User.create({
+        name: 'Genny',
+        total_size: 20
+      });
+    });
+    
   
     return User;
   };
