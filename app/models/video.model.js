51694c2102e9e50106ded654767ebda13e7e938d
MichaelWang <lyvanna1987@outlook.com>
2022-05-02 01:58:24 +0800

    update models

--- app/models/video.model.js
+++ app/models/video.model.js
@@ -6,26 +6,7 @@ module.exports = (sequelize, Sequelize) => {
       video_id: {
         type: Sequelize.INTEGER
       }
-    });
-
-    // Video.sync().then(() => {
-    //   Video.create({
-    //     user_id: 1,
-    //     video_id: 1
-    //   });
-    //   Video.create({
-    //     user_id: 1,
-    //     video_id: 2
-    //   });
-    //   Video.create({
-    //     user_id: 2,
-    //     video_id: 3
-    //   });
-    //   Video.create({
-    //     user_id: 3,
-    //     video_id: 4
-    //   });
-    // });
+    });   
   
     return Video;
 };
